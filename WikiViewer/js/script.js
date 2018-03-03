98e7c4715df0184d0ac3532dd6d077bb9602683f
Marci <martonlanga@gmail.com>
2018-03-03 22:01:24 +0100

    Started project WikiViewer

---
+++ WikiViewer/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+});
+
+//After clicking away from input, stop "Search" to transform back to original position
+function stopMovingBack() {
+  console.log("sdf")
+  $(".effect~label").css("top", "-12px");
+  $(".effect~label").css("font-size", "70%");
+}
