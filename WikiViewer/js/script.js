ac9e643633bfa99b1d69fb322ca5a05d946e6fca
Marci <martonlanga@gmail.com>
2018-03-04 11:16:58 +0100

    Finished styling of search

--- WikiViewer/js/script.js
+++ WikiViewer/js/script.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
-
+  $('#input').keyup(function(e){
+      if(e.keyCode == 13 && $(this).val().length > 1)
+      {
+        console.log('123');
+      }
+  });
 });
 
 //After clicking away from input, stop "Search" to transform back to original position
 function stopMovingBack() {
-  console.log("sdf")
-  $(".effect~label").css("top", "-12px");
-  $(".effect~label").css("font-size", "70%");
+  $(".effect~label").css("top", "-14px");
+  $(".effect~label").css("font-size", "90%");
+}
+
+function openRandom() {
+  window.open("https://en.wikipedia.org/wiki/Special:Random");
 }
