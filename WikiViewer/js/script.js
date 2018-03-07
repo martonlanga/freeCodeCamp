4a929ee48e2817d7015b23740209c745844dc081
Marci <martonlanga@gmail.com>
2018-03-07 19:38:16 +0100

    Redesigned with flexbox

--- WikiViewer/js/script.js
+++ WikiViewer/js/script.js
@@ -1,24 +1,30 @@
 $(document).ready(function() {
-  $('#input').keyup(function(e) {
+  $('.input').keyup(function(e) {
     if (e.keyCode == 13 && $(this).val().length > 1) {
       getData($(this).val());
-      $(".search").css("top", "7%");
-      $(".search").css("transition", "0.4s");
+
+      var width = $(window).width();
+      console.log(width);
+      if (width >= 1600) {
+        $(".intro").css("margin-bottom", "42%");
+      } else if (width < 1600 && width > 1200) {
+        $(".intro").css("margin-bottom", "42%");
+      } else if (width <= 1200 && width > 900) {
+        $(".intro").css("margin-bottom", "52%");
+      } else if (width <= 900 && width > 768) {
+        $(".intro").css("margin-bottom", "72%");
+      } else if (width <= 768 && width > 480) {
+        $(".intro").css("margin-bottom", "88%");
+      } else {
+        $(".intro").css("margin-bottom", "130%");
+        console.log(width);
+
+      }
+
+      $(".intro").css("transition", "0.3s");
     }
   });
 });
-
-//After clicking away from input, stop "Search" to transform back to original position
-function stopMovingBack() {
-  $(".effect~label").css("top", "-14px");
-  $(".effect~label").css("font-size", "90%");
-}
-
-//Open random article
-function openRandom() {
-  window.open("https://en.wikipedia.org/wiki/Special:Random");
-}
-
 
 function getData(search) {
   $.getJSON('https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search=' +
@@ -30,5 +36,5 @@ function getData(search) {
 }
 
 function processData (data) {
-  
+
 }
