02f492c470f8ae7a968e0d190d35c9ea956d30d9
Marci <martonlanga@gmail.com>
2018-03-11 14:26:55 +0100

    Finished WikiViewer

--- WikiViewer/js/script.js
+++ WikiViewer/js/script.js
@@ -3,25 +3,9 @@ $(document).ready(function() {
     if (e.keyCode == 13 && $(this).val().length > 1) {
       getData($(this).val());
 
-      var width = $(window).width();
-      console.log(width);
-      if (width >= 1600) {
-        $(".intro").css("margin-bottom", "42%");
-      } else if (width < 1600 && width > 1200) {
-        $(".intro").css("margin-bottom", "42%");
-      } else if (width <= 1200 && width > 900) {
-        $(".intro").css("margin-bottom", "52%");
-      } else if (width <= 900 && width > 768) {
-        $(".intro").css("margin-bottom", "72%");
-      } else if (width <= 768 && width > 480) {
-        $(".intro").css("margin-bottom", "88%");
-      } else {
-        $(".intro").css("margin-bottom", "130%");
-        console.log(width);
-
-      }
-
-      $(".intro").css("transition", "0.3s");
+      $(".intro").css("margin-bottom", "2%");
+      $(".intro").css("margin-top", "2%");
+      $(".intro").css("transition", "0.5s");
     }
   });
 });
@@ -35,6 +19,42 @@ function getData(search) {
     });
 }
 
-function processData (data) {
+function processData(data) {
+  var headings = data[1];
+  var descriptions = cropDescription(data[2]);
+  var links = data[3];
 
+  var html = '';
+
+  for (var i = 0; i < headings.length; i++) {
+    html +=
+      '<a href=\"' + links[i] +
+      '\" class=\"list-group-item list-group-item-action\">' +
+      '<div class=\"d-flex w-100\">' +
+      '<h3 class=\"">' + headings[i] + '</h3>' +
+      '</div>' +
+      '<div class=\"d-flex\">' +
+      '<p>' + descriptions[i] + '</p>' +
+      '</div>' +
+      '</a>'
+  }
+
+  showData(html)
+}
+
+function showData(html) {
+  $(".list-group").html(html);
+}
+
+function cropDescription(descriptions) {
+  var croppedDescriptions = [];
+  descriptions.forEach(function(d) {
+    if (d.length > 50) {
+      croppedDescriptions.push(d.substring(0, 100) + '...');
+    } else {
+      croppedDescriptions.push(d);
+    }
+  });
+
+  return croppedDescriptions;
 }
