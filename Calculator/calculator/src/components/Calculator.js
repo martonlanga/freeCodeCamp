9ab671e823f8bc626248db8cf3ed210ac7088d41
Marci <martonlanga@gmail.com>
2018-04-19 22:43:45 +0200

    Added buttons

--- Calculator/calculator/src/components/Calculator.js
+++ Calculator/calculator/src/components/Calculator.js
@@ -3,7 +3,22 @@ import React from 'react';
 class Calculator extends React.Component {
   render() {
     return (
-      <h1>Hello</h1>
+      <div>
+        <Display></Display>
+
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
+      </div>
     );
   }
 }
