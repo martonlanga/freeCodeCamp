3cf9e13b60caf8b0fb8b89b35d5ff0663a0c0448
Marci <martonlanga@gmail.com>
2018-04-30 22:23:17 +0200

    Delete unnecessary files

--- Promodoro/promodoro/src/index.js
+++ Promodoro/promodoro/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Promodoro from './Promodoro';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Promodoro />, document.getElementById('root'));
 registerServiceWorker();
