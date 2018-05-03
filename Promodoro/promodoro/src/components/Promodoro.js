8ae2edc0086174d96b7c68088c19ea4b526654e3
Marci <martonlanga@gmail.com>
2018-05-03 07:44:39 +0200

    Added icons on AppBar

--- Promodoro/promodoro/src/components/Promodoro.js
+++ Promodoro/promodoro/src/components/Promodoro.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import Countdown from './Countdown';
 import AppBar from 'material-ui/AppBar';
+import AlarmIcon from 'material-ui/svg-icons/action/alarm';
+import {white} from 'material-ui/styles/colors';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
 import './Promodoro.css';
 
 class Promodoro extends React.Component {
@@ -24,6 +30,20 @@ class Promodoro extends React.Component {
         <AppBar
           title='Promodoro Clock'
           className='appbar'
+          iconElementLeft={<AlarmIcon color={white} />}
+          iconElementRight={
+            <IconMenu
+              iconButtonElement={
+                <IconButton><MoreVertIcon /></IconButton>
+              }
+              targetOrigin={{horizontal: 'right', vertical: 'top'}}
+              anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+            >
+              <MenuItem primaryText='Work time' />
+              <MenuItem primaryText='Break time' />
+              <MenuItem primaryText='Dark theme' />
+            </IconMenu>
+          }
         />
         <Countdown
           breakTime={breakTime}
